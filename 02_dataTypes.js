3f57bbc4e477e015de025c3377769ce9330e02ee
ashishiitbhu27 <ashish027840@gmail.com>
2024-10-14 14:31:13 +0530

    Create 02_dataTypes.js

---
+++ 02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello")  //we are using nodejs, not browser
+
+let name = "Ashish"
+let age = 20
+let isLoggedIn = true
+let temp = null
+let state;
+
+
+//number => 2 to power 53
+//bigint
+
+//string => ""
+//boolean => true/false
+
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof(temp)); // temp is null output is object
+console.log(typeof(undefined)); // output is undefined
