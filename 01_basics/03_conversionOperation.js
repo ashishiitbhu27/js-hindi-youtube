a2b9d894d88d2aa5b275089fd4cf25f4d129620a
ashishiitbhu27 <ashish027840@gmail.com>
2024-11-25 09:11:13 +0000

    03_conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+// let score = "33asd"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log((valueInNumber));   //NaN
+
+// let score = null
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)      //0
+
+// let score = undefined
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)      //NaN
+
+// let score = true
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)      //1
+
+// let score = "Ashish"
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)      //NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)  // true.
+
+// 1=> true; 0=> false
+// "" => false
+// "Ashish" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+ 
